da68ee4e2aabef63efc9af8daa58059d4f82e17d
Alejandro Figueroa <thejandroman@gmail.com>
2012-11-27 14:22:40 -0500

    Here are the changes

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/Pictures/bing-wallpapers/
-cd ~/Pictures/bing-wallpapers/
+###
+# Requires wget; use homebrew if not installed
+###
 
-curl "http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US" | grep -o 'url="[^"]*"' | sed -e 's/url="\([^"]*\)"/\1/' | sed -e "s/ /%20/g" > tmp.txt
+PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 
-#read file line
-while read line
-do
-    fileName=$(echo $line | sed -e "s;.*/\([^\/]*\)$;\1;")
-    if [[ -f $fileName ]]; then
-        echo "$fileName already exists" > log.txt
+mkdir -p $PICTURE_DIR
+
+urls=( $(curl -s http://www.bing.com|grep -Eo "url:'.*?'"|sed -e "s/url:\'\(.*\)\'/http:\/\/bing.com\1/"|sed -e "s/\\\//g") )
+
+for p in ${urls[@]}; do
+    filename=$(echo $p|sed -e "s/.*%2f\(.\)/\1/")
+    if [ ! -f $PICTURE_DIR/$filename ]; then
+        echo "Downloading: $filename ..."
+        wget -q -O $PICTURE_DIR/$filename $p
     else
-        curl -O $line;
+        echo "Skipping: $filename ..."
     fi
-done < "tmp.txt"
+done
