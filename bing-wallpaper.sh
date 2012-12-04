a06a27c5c79a630c64052f629f9a78d0d63d49dd
ktmud <jyyjcc@gmail.com>
2012-12-04 10:48:36 +0800

    new theme urls from MS

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -1,6 +1,8 @@
 #!/usr/bash
 cd /Users/jesse/projects/bing-wallpapers/
-curl "http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US" | grep -o 'url="[^"]*"' | sed -e 's/url="\([^"]*\)"/\1/' | sed -e "s/ /%20/g" > tmp.txt
+# for more urls, goto http://windows.microsoft.com/en-US/windows/themes?T1=rss
+# download a theme, use a text editor to open it, and find the url.
+curl "http://themeserver.microsoft.com/default.aspx?p=Windows&c=Flora&m=en-US" | grep -o 'url="[^"]*"' | sed -e 's/url="\([^"]*\)"/\1/' | sed -e "s/ /%20/g" > tmp.txt
 
 #read file line
 while read line
