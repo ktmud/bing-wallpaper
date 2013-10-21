5a03420c1bc5effdec0659698a040cab10ca302b
Hwasung Lee <hwasung.lee@oriel.oxon.org>
2013-10-21 14:13:09 +0100

    Minor reformatting so that it is easier to read the code.

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -4,13 +4,15 @@ PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 
 mkdir -p $PICTURE_DIR
 
-urls=( $(curl -s http://www.bing.com|grep -Eo "url:'.*?'"|sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/"|sed -e "s/\\\//g") )
+urls=( $(curl -s http://www.bing.com | \
+    grep -Eo "url:'.*?'" | \
+    sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" | \
+    sed -e "s/\\\//g") )
 
 for p in ${urls[@]}; do
     filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
     if [ ! -f $PICTURE_DIR/$filename ]; then
         echo "Downloading: $filename ..."
-        #wget -q -O $PICTURE_DIR/$filename $p
         curl -Lo "$PICTURE_DIR/$filename" $p
     else
         echo "Skipping: $filename ..."
